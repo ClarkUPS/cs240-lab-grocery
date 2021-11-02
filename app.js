622eaeea81265f252312a2228a6472b34cd3c4a6
ClarkUPS <90676910+ClarkUPS@users.noreply.github.com>
2021-11-01 22:57:38 -0700

    to github

--- app.js
+++ app.js
@@ -1,14 +1,18 @@
 /** add an event for keydown */
 let inputNode = document.querySelector("input[name='itemName']");
+
 inputNode.addEventListener("keydown", function (evt) {
   // need to check if the return key was depressed
   if (evt.code === "Enter") {
     // create a <span>value from input field</span>
     let itemName = document.createElement("span");
+    let boxName = document.createElement("input");
+    boxName.type = "checkBox";
     itemName.innerHTML = inputNode.value;
 
     // nest the <span> in <li>...</li>
     let newListItem = document.createElement("li");
+    newListItem.appendChild(boxName);
     newListItem.appendChild(itemName);
 
     // append the <li> to the end of the list
